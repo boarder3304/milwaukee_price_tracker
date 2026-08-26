45f28cbdf8da609784e52096aab1b215298b4c43
boarder3304 <adam.sexton@gmail.com>
2026-08-26 15:52:06 -0400

    Update config.py

--- config.py
+++ config.py
@@ -24,3 +24,6 @@ USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
     "(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 )
+
+#SERP API
+SERPAPI_KEY = os.environ.get("SERPAPI_KEY", "")
