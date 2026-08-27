be23b5cd8d1739dd8fb8cb674de8f49b942d01e5
boarder3304 <adam.sexton@gmail.com>
2026-08-26 21:46:34 -0400

    Update __init__.py

--- retailers/__init__.py
+++ retailers/__init__.py
@@ -9,7 +9,7 @@ CSS selector needs updating - check the "error" field for a hint.
 """
 from urllib.parse import urlparse
 
-from . import homedepot, milwaukeetool, redtoolstore acehardware, generic
+from . import homedepot, milwaukeetool, redtoolstore, acehardware, generic
 
 
 def get_fetcher(url: str):
